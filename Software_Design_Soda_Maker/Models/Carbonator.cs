c4b00bfc4569d6724eaa797583db6aa6ffce9d31
JScrivo <scrivjor@sheridancollege.ca>
2020-11-27 17:00:27 -0500

    Started Implemented Carbonator

---
+++ Software_Design_Soda_Maker/Models/Carbonator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Software_Design_Soda_Maker.Models
+{
+    public class Carbonator
+    {
+        private CO2Regulator co2Regulator;
+        private WaterRegulator waterRegulator;
+        public Water water { get; private set; }
+        private CO2 cO2;
+
+        public Carbonator(CO2Regulator coReg, WaterRegulator waterReg)
+        {
+            co2Regulator = coReg;
+            waterRegulator = waterReg;
+        }
+
+        public void carbonateWater()
+        {
+
+        }
+    }
+}
